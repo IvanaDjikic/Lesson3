cd4c7967b55a0f8bf55dc89f93e6f3a592e572e5
IvanaDjikic <ivdjikic@gmail.com>
2019-04-25 01:00:14 +0200

    color chooser change

--- src/optional/RobotColorChooser.java
+++ src/optional/RobotColorChooser.java
@@ -3,20 +3,54 @@ package optional;
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
+import sun.security.util.PendingException;
+
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		Robot r = new Robot();
+		int x = 200;
+		int y = 200;
+		r.setSpeed(100);
 
-		// 3. ask the user what color they would like the tortoise to draw
+		for (int i = 0; i < 6; i++) {
 
-		// 4. use an if/else statement to set the pen color that the user requested
+			String answer = JOptionPane.showInputDialog("What color would you like the tortoise to draw?");
 
-		// 5. if the user doesn’t enter anything, choose a random color
+			if (answer.contentEquals("red")) {
+				r.setPenColor(255, 0, 0);
+			} else if (answer.contentEquals("green")) {
+				r.setPenColor(0, 128, 0);
+			} else if (answer.contentEquals("yellow")) {
+				r.setPenColor(255, 255, 0);
+			} else if (answer.contentEquals("blue")) {
+				r.setPenColor(0, 0, 255);
+			} else if (answer.contentEquals("black")) {
+				r.setPenColor(0, 0, 0);
+			} else if (answer.contentEquals("white")) {
+				r.setPenColor(255, 255, 255);
+			} else if (answer.contentEquals("magenta")) {
+				r.setPenColor(255, 0, 255);
+			} else if (answer.contentEquals("cyan")) {
+				r.setPenColor(0, 255, 255);
+			}
 
-		// 6. put a loop around your code so that you keep asking the user for more colors & drawing them
+			else {
+				r.setRandomPenColor();
+			}
 
-		// 2. set the pen width to 10
+			r.setPos(x, y);
+			r.setPenWidth(10);
 
-		// 1. make the robot draw a shape (this will take more than one line of code)
+			r.penDown();
+
+			for (int n = 0; n < 4; n++) {
+				r.move(200);
+				r.turn(90);
+			}
+			x += 20;
+			y += 20;
+
+		}
 
 	}
 }
