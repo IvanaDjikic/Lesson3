374dbf14512311b72281621a0a6ecf704d85d2fa
IvanaDjikic <ivdjikic@gmail.com>
2019-04-30 14:53:33 +0200

    blocklyBirdExcersise change

--- src/optional/BlocklyBirdExercise.java
+++ src/optional/BlocklyBirdExercise.java
@@ -5,3 +5,77 @@ package optional;
 */
 
 public class BlocklyBirdExercise {}
+
+/* level 1 
+ * heading(45);
+ * 
+ * level 2
+ * if (noWorm()) {
+  heading(0);
+} else {
+  heading(90);
+}
+
+level 3
+if (noWorm()) {
+  heading(315);
+} else {
+  heading(45);
+}
+
+level 4
+
+if (getX() > 70) {
+  heading(270);
+} else {
+  heading(0);
+}
+
+level 5
+if (getY() < 20) {
+  heading(180);
+} else {
+  heading(270);
+}
+
+level 6
+if (noWorm()) {
+  heading(330);
+} else if (getY() < 80) {
+  heading(90);
+} else {
+  heading(180);
+}
+
+level 7
+if (getY() > 50) {
+  heading(225);
+} else if (noWorm()) {
+  heading(315);
+} else {
+  heading(180);
+}
+
+level 8
+if (getX() < 50) {
+  heading(45);
+} else if (noWorm()) {
+  heading(315);
+} else if (getX() > 50 && getY() < 50) {
+  heading(135);
+} else {
+  heading(45);
+}
+
+level 9
+if (getY() > 60 && getX() > 20) {
+  heading(180);
+} else if (noWorm()) {
+  heading(270);
+} else if (getY() < 60 && getX() < 30) {
+  heading(75);
+} else if (getX() < 60) {
+  heading(0);
+} else {
+  heading(300);
+}*/
